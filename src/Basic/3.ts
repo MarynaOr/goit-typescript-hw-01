f6ec0e726b780c78d10f6bfb960603e2eaccf6e6
Maryna <89296066105marina@gmal.com>
2025-05-21 13:03:41 +0200

    Basic

---
+++ src/Basic/3.ts
@@ -0,0 +1,7 @@
+let variableF: string | number = 'hello';
+let variableS: 'enable' | 'disable' = 'disable'
+
+variableF = 'hello'
+variableS ='enable'
+console.log(variableF);
+console.log(variableS);
